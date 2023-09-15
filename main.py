29106ac28a2011a8a823d31e0f78f7d9c0cc066b
jsuazo <javier2587@gmail.com>
2023-09-15 19:13:41 -0300

    Fin operaciones infinitas

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def create_window(theme):
     button_size = (6, 3)
 
     layout = [
-        [sg.Text('output', 
+        [sg.Text('0', 
                  font = 'Franklin 26', 
                  justification = 'right', 
                  expand_x = True, 
@@ -22,7 +22,7 @@ def create_window(theme):
         [sg.Button(7, size = button_size), sg.Button(8, size = button_size), sg.Button(9, size = button_size), sg.Button('*', size = button_size)],
         [sg.Button(4, size = button_size), sg.Button(5, size = button_size), sg.Button(6, size = button_size), sg.Button('/', size = button_size)],
         [sg.Button(1, size = button_size), sg.Button(2, size = button_size), sg.Button(3, size = button_size), sg.Button('-', size = button_size)],
-        [sg.Button(0, expand_x = True), sg.Button(',', size = button_size), sg.Button('+', size = button_size)]
+        [sg.Button(0, expand_x = True), sg.Button('.', size = button_size), sg.Button('+', size = button_size)]
         ]
     return sg.Window('Calculadora', layout)
 
@@ -32,6 +32,7 @@ window = create_window('LightGrey1')
 
 current_num = []
 full_operation = []
+result = ''
 
 while True:
     event, values = window.read()
@@ -42,7 +43,7 @@ while True:
         window.close()
         window = create_window(event)
 
-    if event in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',']:
+    if event in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']:
         current_num.append(event)
         num_string = ''.join(current_num)
         window['-TEXT-'].update(num_string)
@@ -51,12 +52,20 @@ while True:
         full_operation.append(''.join(current_num))
         current_num = []
         full_operation.append(event)
-        print(full_operation)
+        window['-TEXT-'].update('')
 
     if event == 'Enter':
-        print(event)
+        full_operation.append(''.join(current_num))
+        result = eval(' '.join(full_operation))
+        window['-TEXT-'].update(result)
+        current_num = list(map(str, str(result)))
+        full_operation = []
+        
 
     if event == 'C':
-        print(event)
+        current_num = []
+        full_operation = []
+        result = []
+        window['-TEXT-'].update('0')     
 
 window.close()
